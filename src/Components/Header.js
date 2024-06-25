61ee5b247e7243f13f2d2f0876c143117fce3e61
Deekshith Shetty <deekshithshetty698@gmail.com>
2024-06-25 07:29:57 +0530

    Add links and hover effects to the header section

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -18,14 +18,51 @@ export default function Header() {
       </div>
       <div className={`header-info burger ${hamburger && "hidden"}`}>
         <div className="header-links">
-          <p className="project-link">Projects</p>
-          <p className="about-me">About Me</p>
-          <p className="contact">Contact</p>
+          <p className="project-link">
+            <a className="scroll-link" href="#projects">
+              Projects
+            </a>
+          </p>
+          <p className="about-me">
+            <a className="scroll-link" href="#about">
+              About Me
+            </a>
+          </p>
+          <p className="contact">
+            <a className="scroll-link" href="#contact">
+              Contact
+            </a>
+          </p>
         </div>
         <div className="header-icons">
-          <img src="./images/linkedin.png" alt="Linkedin" className="h-icons" />
-          <img src="./images/x.png" alt="x" className="h-icons" />
-          <img src="./images/github.png" alt="github" className="h-icons" />
+          <a
+            href="https://www.linkedin.com/in/deekshith-shetty-3a4138224"
+            className="scroll-link"
+            target="_blank"
+            rel={"noreferrer"}
+          >
+            <img
+              src="./images/linkedin.png"
+              alt="Linkedin"
+              className="h-icons"
+            />
+          </a>
+          <a
+            href="https://x.com/Deekshi13971032"
+            className="scroll-link"
+            target="_blank"
+            rel={"noreferrer"}
+          >
+            <img src="./images/x.png" alt="x" className="h-icons" />
+          </a>
+          <a
+            href="https://github.com/deekshith-S-Shetty/"
+            className="scroll-link"
+            target="_blank"
+            rel={"noreferrer"}
+          >
+            <img src="./images/github.png" alt="github" className="h-icons" />
+          </a>
         </div>
       </div>
     </div>
